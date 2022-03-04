b7cf622d58b0b541dab234e34574713337ae53e3
qchantel <qchantelot@gmail.com>
2022-03-04 20:10:28 +0100

    detached mod

--- start-prod-server.sh
+++ start-prod-server.sh
@@ -1,1 +1,1 @@
-docker-compose -f docker-compose.yml -f docker-compose.production.yml up --no-deps --build
+docker-compose -f docker-compose.yml -f docker-compose.production.yml up --no-deps --build -d
