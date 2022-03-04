290fdaedc40cb323ef19aab2b17384bb9cb25778
qchantel <qchantelot@gmail.com>
2022-03-04 16:22:19 +0100

    update scripts with prod config

--- build-and-exec-docker-dev.sh
+++
@@ -1,3 +0,0 @@
-docker build -t micro-parse-dev-docker -f Dockerfile.development .
-docker run -p 49160:8080 -d micro-parse-dev-docker
-
--- build-and-exec-docker.sh
+++
@@ -1,2 +0,0 @@
-docker build . -t qchantel/micro-parse-web-appo
-docker run -p 49160:8080 -d qchantel/micro-parse-web-app
